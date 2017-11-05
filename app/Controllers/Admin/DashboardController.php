0836a2599d4a27272460259e4a53022230473856
GolovaVasilenko <vasilenko75@gmail.com>
2017-11-05 18:49:40 +0200

    create AdminController and DashboardController

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alexey
- * Date: 05.11.17
- * Time: 18:47
- */
+
 
 namespace app\Controllers\Admin;
 
 
-class DashboardController {
+class DashboardController extends AdminController
+{
 
 }
